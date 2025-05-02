feda3fa18c36777b32fbbdcab45117395b7b6a49
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-05-02 10:34:50 -0600

    solve warnings

--- app/src/main/java/com/moviles/taskmind/components/CourseCard.kt
+++ app/src/main/java/com/moviles/taskmind/components/CourseCard.kt
@@ -1,12 +1,10 @@
 package com.moviles.taskmind.components
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.Person
