d44e345fe187757195b7b61f5951e6796580ba44
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-05-02 10:31:55 -0600

    view CoursePage with CourseCard

--- app/src/main/java/com/moviles/taskmind/pages/CoursePage.kt
+++ app/src/main/java/com/moviles/taskmind/pages/CoursePage.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.moviles.taskmind.components.CourseCard
 
 @Composable
 fun CoursePage(modifier: Modifier = Modifier) {
@@ -19,8 +20,22 @@ fun CoursePage(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Esta es la pagina de los cursos",
-            fontSize = 20.sp,
-            fontWeight = FontWeight.Bold)
-    }
+        CourseCard(
+            title = "Diseño y programación de plataformas móviles",
+            professor = "Prof. Rachel Bolívar Morales",
+            progressBar = 0.75f.times(100).toInt(),
+            event = "Examen Parcial ",
+             progressColor ="#9C27B0",
+            colorMain = "#DBEAFE"
+        )
+//0xFF9C27B0
+        CourseCard(
+            title = "Métodos de Investigación Científica en Informática",
+            professor = "Prof. Willy Pineda Lizano",
+            progressBar = 75,
+            event = "Avance Proyecto",
+            progressColor ="#FF9800",
+            colorMain = "#DBEAFE"
+        )
+    }//0xFFFF9800
 }
