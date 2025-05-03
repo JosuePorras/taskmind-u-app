9d093c7444bc2d32f0de3a1f589ddc4600ca22e1
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-05-02 20:49:39 -0600

    implement scroll in CoursePage

--- app/src/main/java/com/moviles/taskmind/pages/CoursePage.kt
+++ app/src/main/java/com/moviles/taskmind/pages/CoursePage.kt
@@ -2,37 +2,87 @@ package com.moviles.taskmind.pages
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.moviles.taskmind.components.CourseCard
 
 @Composable
 fun CoursePage(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.fillMaxSize().background(Color(0xFFA2E0A4)),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+    val scrollState = rememberScrollState()
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.White)
     ) {
-        CourseCard(
-            title = "Diseño y programación de plataformas móviles",
-            professor = "Prof. Rachel Bolívar Morales",
-            progressBar = 0.75f.times(100).toInt(),
-            event = "Examen Parcial ",
-             progressColor ="#9C27B0",
-            colorMain = "#DBEAFE"
-        )
-//0xFF9C27B0
-        CourseCard(
-            title = "Métodos de Investigación Científica en Informática",
-            professor = "Prof. Willy Pineda Lizano",
-            progressBar = 75,
-            event = "Avance Proyecto",
-            progressColor ="#FF9800",
-            colorMain = "#DBEAFE"
-        )
-    }//0xFFFF9800
+        Column {
+
+            /*falta crear un hearer con el formulario (Aaron)*/
+
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                val cardModifier = Modifier.padding(vertical = 4.dp)
+
+                CourseCard(
+                    title = "Diseño y programación de plataformas móviles",
+                    professor = "Prof. Rachel Bolívar Morales",
+                    progressBar = (0.75f * 100).toInt(),
+                    event = "Examen Parcial",
+                    progressColor = "#9C27B0",
+                    colorMain = "#DBEAFE"
+                )
+
+                CourseCard(
+                    title = "Métodos de Investigación Científica en Informática",
+                    professor = "Prof. Willy Pineda Lizano",
+                    progressBar = 75,
+                    event = "Avance Proyecto",
+                    progressColor = "#FF9800",
+                    colorMain = "#FEF3C7"
+                )
+
+                CourseCard(
+                    title = "Ingenieria en Sistemas III",
+                    professor = "Prof. Michael Barquero",
+                    progressBar = (0.75f * 100).toInt(),
+                    event = "Sprint-4",
+                    progressColor = "#10B981",
+                    colorMain = "#DCFCE7"
+                )
+
+                CourseCard(
+                    title = "Algebra Lineal",
+                    professor = "Prof. Julio Marin",
+                    progressBar = 75,
+                    event = "Examen Parcial II",
+                    progressColor = "#EF4444",
+                    colorMain = "#FEE2E2"
+                )
+            }
+        }
+    }
 }
+
+
+
+
+
