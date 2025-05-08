2e0401a5c3b105c28fc2b5d6154fef198e472782
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-05-08 01:08:15 -0600

    create color fun

--- app/src/main/java/com/moviles/taskmind/utils/ColorUtils.kt
+++ app/src/main/java/com/moviles/taskmind/utils/ColorUtils.kt
@@ -1,10 +1,21 @@
 package com.moviles.taskmind.utils
 
 import androidx.compose.ui.graphics.Color
+import androidx.core.graphics.ColorUtils
 
 fun parseColorString(colorString: String): Color {
     return try {
         Color(android.graphics.Color.parseColor(colorString))
+    } catch (e: IllegalArgumentException) {
+        Color.Gray
+    }
+}
+
+fun darkenColorHex(hexColor: String, factor: Float = 0.8f): Color {
+    return try {
+        val original = android.graphics.Color.parseColor(hexColor)
+        val darkened = ColorUtils.blendARGB(original, android.graphics.Color.BLACK, 1 - factor)
+        Color(darkened)
     } catch (e: IllegalArgumentException) {
         Color.Gray
     }
