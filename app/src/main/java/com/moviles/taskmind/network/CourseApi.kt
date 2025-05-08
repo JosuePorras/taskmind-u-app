e43422846a18caa55a94989c42f8809ab8835961
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-05-08 01:08:37 -0600

    update getCourses fun

--- app/src/main/java/com/moviles/taskmind/network/CourseApi.kt
+++ app/src/main/java/com/moviles/taskmind/network/CourseApi.kt
@@ -2,14 +2,16 @@ package com.moviles.taskmind.network
 
 import com.moviles.taskmind.models.Course
 import com.moviles.taskmind.models.CourseResponse
+import com.moviles.taskmind.models.GetCourseResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface CourseApi {
     @GET("api/courses/all_course")
-    suspend fun getCourses(): List<Course>
+    suspend fun getCourses( @Query("userId") userId: Int): GetCourseResponse
 
     @POST("api/courses/register")
     suspend fun addCourse(@Body course: Course): Response<CourseResponse>
