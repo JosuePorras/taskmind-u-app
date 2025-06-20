0ed1605c74bcd44e692ffa2e697c71dc4696ce09
YeilerMR <yeiler.montes.rojas@est.una.ac.cr>
2025-06-20 09:05:11 -0600

    update Constant

--- app/src/main/java/com/moviles/taskmind/common/Constants.kt
+++ app/src/main/java/com/moviles/taskmind/common/Constants.kt
@@ -1,6 +1,15 @@
 package com.moviles.taskmind.common
 
 object Constants {//10.0.2.2:5275
-    const val API_BASE_URL = "http://192.168.1.132:4000/"
+    //IP = Aaron
+    //const val API_BASE_URL = "http://192.168.1.132:4000/"
+
+    //IP = UNA
+    //const val API_BASE_URL = "http://172.17.50.121:4000/"
+
+    //IP = Yeiler
+    const val API_BASE_URL = "http://10.0.2.2:4000/"
+
+
   //  const val IMAGES_BASE_URL = API_BASE_URL + "uploads/"
 }
